9bd46ab3a238d558417fbaef95b34b10e0585c9d
heyvishusri <vishwaskumar55347@gmail.com>
2025-04-21 02:27:36 +0530

    Add project files.

---
+++ Student_Portal/Student_Portal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Portal.Web.Models.Entities;
+
+namespace Student_Portal.Web.Data
+{
+    public class ApplicationDbContext : DbContext      //DbContext: serves as the central class for interacting with the database
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+       
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
